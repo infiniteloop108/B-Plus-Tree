2155388737a9b179450d929f35c0c8c97b220206
infiniteloop <aksagg@iitk.ac.in>
2015-04-01 15:23:14 +0530

    can now read/write files

--- code.cpp
+++ code.cpp
@@ -55,7 +55,40 @@ struct node
 	vector<int> keys;	//keys stored
 	vector<string> children;	//Children pointers
 };
-
+//Write this node to its file
+void writeNode(node x)
+{
+	ofstream fout(x.fName, ios::out);
+	fout<<x.isRoot<<endl;
+	fout<<x.isLeaf<<endl;
+	fout<<x.numKeys<<endl;
+	for(int i=0;i<x.numKeys;++i)
+		fout<<x.keys[i]<<" ";
+	fout<<endl;
+	for(int i=0;i<=x.numKeys;++i)
+		fout<<x.children[i]<<" ";
+}
+//Write the data in a file
+void writeData(string fName, string data)
+{
+	ofstream fout(fName,ios::out);
+	fout<<data;
+	fout.close();
+}
+//Read a node from a file
+node readNode(string fName)
+{
+	node ans;
+	ifstream fin(fName, ios::in);
+	fin>>ans.isRoot;
+	fin>>ans.isLeaf;
+	fin>>ans.numKeys;
+	ans.keys.resize(ans.numKeys);
+	ans.children.resize(ans.numKeys+1);
+	for(int i=0;i<ans.numKeys;++i)fin>>ans.keys[i];
+	for(int i=0;i<=ans.numKeys;++i)fin>>ans.children[i];
+	return ans;
+}
 int main()
 {
 	readMaxKeys();
